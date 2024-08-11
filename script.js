9ea1beecbb8de520c06c9041bd3fa7e1b8e60320
Vinicius Morais <145579046+Sousasz@users.noreply.github.com>
2024-08-11 20:22:09 -0300

    Removed comments in code

--- script.js
+++ script.js
@@ -8,28 +8,3 @@ fetch("dados.json").then((response) => {
     });
   });
 });
-
-// Fetch é utilizado para fazer a requisição de informações
-
-/*
-O JavaScript utiliza o 'fetch' para fazer a requisição dos
-dados do arquivo chamado "dados.json". Em seguida/ou entao
-(.then), é gerada uma resposta e deve ser do tipo .json.
-Em seguida, pega os valores presentes em usuários
-*/
-
-/*
-Quando aplicamos o termo 'dados' na requisição, estamos pegando
-os dados como um todo, já que o array 'usuários' está dentro de
-um objeto
-*/
-
-/*
-Utilizando a props. map, definimos um nome para cada elemento do nosso
-objeto, no caso temos um array usuários, onde cada posição é um usuário
-*/
-
-/*
-Quando definimos "+=", trazemos a ideia de incremento, ou seja, no HTML será aplicado a informação
-que está dentro dele junto com outro elemento 
-*/
